005429a7d7e71f51c3e08dd307da4b09edc385a6
Daniel Below <dbelow@hotmail.de>
2013-04-29 17:23:40 +0200

    Basic Project Skeleton
    Added stdheader and main.
    Added .gitignore.
    Added solution and set up properties.

---
+++ GameCompilation/GameCompilation/main.c
@@ -0,0 +1,9 @@
+#include "stdheader.h"
+
+int main()
+{
+
+	fflush(stdin);
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
---
+++ GameCompilation/GameCompilation/stdheader.h
@@ -0,0 +1,7 @@
+#ifndef STDHEADER_H_INCLUDED
+#define STDHEADER_H_INCLUDED
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif //STDHEADER_H_INCLUDED
