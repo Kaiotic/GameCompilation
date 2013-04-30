859bdf79e5af8319ad6d756be1d333dc3a4d9fad
Daniel Below <dbelow@hotmail.de>
2013-04-30 12:53:18 +0300

    Update Vec2df32.c
    Comments.

--- GameCompilation/GameCompilation/Vec2df32.c
+++ GameCompilation/GameCompilation/Vec2df32.c
@@ -1,30 +1,79 @@
 #include "Vec2df32.h"
 #include <math.h>
 
+/*****************************************************************************
+Vec2df32_t addVec(const Vec2df32_t _First, const Vec2df32_t _Second)
+	Description :
+		Adds two given vectors.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+		const Vec2df32_t _Second: Second vector.
+	Returns :
+		Vector-sum of the given two vectors.
+*****************************************************************************/
 Vec2df32_t addVec(const Vec2df32_t _First, const Vec2df32_t _Second)
 {
   Vec2df32_t vec = { _First.iX + _Second.iX, _First.iY + _Second.iY };
 	return vec;
 }
 
+/*****************************************************************************
+Vec2df32_t subVec(const Vec2df32_t _First, const Vec2df32_t _Second)
+	Description :
+		Subtracts two given vectors.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+		const Vec2df32_t _Second: Second vector.
+	Returns :
+		Vector-difference of the given two vectors.
+*****************************************************************************/
 Vec2df32_t subVec(const Vec2df32_t _First, const Vec2df32_t _Second)
 {
 	Vec2df32_t vec = { _First.iX - _Second.iX, _First.iY - _Second.iY };
 	return vec;
 }
 
+/*****************************************************************************
+Vec2df32_t mulVec(const Vec2df32_t _First, const Vec2df32_t _Second)
+	Description :
+		Multiplies two given vectors.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+		const Vec2df32_t _Second: Second vector.
+	Returns :
+		Vector-product of the given two vectors.
+*****************************************************************************/
 Vec2df32_t mulVec(const Vec2df32_t _First, const Vec2df32_t _Second)
 {
 	Vec2df32_t vec = { _First.iX * _Second.iX, _First.iY * _Second.iY };
 	return vec;
 }
 
+/*****************************************************************************
+Vec2df32_t divVec(const Vec2df32_t _First, const Vec2df32_t _Second)
+	Description :
+		Divides two given vectors.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+		const Vec2df32_t _Second: Second vector.
+	Returns :
+		Vector-quotient of the given two vectors.
+*****************************************************************************/
 Vec2df32_t divVec(const Vec2df32_t _First, const Vec2df32_t _Second)
 {
 	Vec2df32_t vec = { _First.iX / _Second.iX, _First.iY / _Second.iY };
 	return vec;
 }
 
+/*****************************************************************************
+Vec2df32_t normalize(const Vec2df32_t _First)
+	Description :
+		Normalizes the given vector.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+	Returns :
+		Normalized vector.
+*****************************************************************************/
 Vec2df32_t normalize(const Vec2df32_t _First)
 {
 	float fFactor = sqrtf(_First.iX * _First.iX + _First.iY * _First.iY);
@@ -33,11 +82,30 @@ Vec2df32_t normalize(const Vec2df32_t _First)
 	return vec;
 }
 
+/*****************************************************************************
+float dotProduct(const Vec2df32_t _First, const Vec2df32_t _Second)
+	Description :
+		Calculates the dot product of the two given vectors.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+		const Vec2df32_t _Second: Second vector.
+	Returns :
+		Dot product of the two given vectors.
+*****************************************************************************/
 float dotProduct(const Vec2df32_t _First, const Vec2df32_t _Second)
 {
 	return (_First.iX * _Second.iX + _First.iY * _Second.iY);
 }
 
+/*****************************************************************************
+Vec2df32_t crossProduct(const Vec2df32_t _First)
+	Description :
+		Calculates the cross product of the given vector.
+	Parameters : 
+		const Vec2df32_t _First: First vector. 
+	Returns :
+		Cross product of the given vector.
+*****************************************************************************/
 Vec2df32_t crossProduct(const Vec2df32_t _First)
 {
 	Vec2df32_t vec = { -(_First.iY), _First.iX };
