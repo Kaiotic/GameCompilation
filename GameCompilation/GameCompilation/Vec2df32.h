a0442fa30f58daf0eb551be5c556faabc8eb8f78
Daniel Below <dbelow@hotmail.de>
2013-04-30 12:42:45 +0300

    Updated Vec2df32
    Added some simple methods that represent basic vector arithmetic.

--- GameCompilation/GameCompilation/Vec2df32.h
+++ GameCompilation/GameCompilation/Vec2df32.h
@@ -7,4 +7,13 @@ typedef struct
 	float iY;
 } Vec2df32_t;
 
+Vec2df32_t addVec(const Vec2df32_t _First, const Vec2df32_t _Second);
+Vec2df32_t subVec(const Vec2df32_t _First, const Vec2df32_t _Second);
+Vec2df32_t mulVec(const Vec2df32_t _First, const Vec2df32_t _Second);
+Vec2df32_t divVec(const Vec2df32_t _First, const Vec2df32_t _Second);
+
+Vec2df32_t normalize(const Vec2df32_t _First);
+float dotProduct(const Vec2df32_t _First, const Vec2df32_t _Second);
+Vec2df32_t crossProduct(const Vec2df32_t _First);
+
 #endif //VEC2DF32_H_INCLUDED
